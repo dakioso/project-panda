532d89e422114b2d32d19c672cfe89694e65af80
Mikael Gustafsson <mikael.gustafsson@student.kyh.se>
2017-10-04 15:37:05 +0200

    add loginbox, loginfunction and link together

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
 // Javascript
 
 // user-array with username & password.
-let student = {username: 'student', password: 'password1'};
-let teacher = {username: 'teacher', password: 'password2'};
+let users = [
+  {username: 'student', password: 'password1'},
+  {username: 'teacher', password: 'password2'}
+];
 
-let users = [student, teacher];
+let button = document.getElementById('loginbutton');
+let loginBox = document.getElementById('login-box');
+let span = document.getElementsByClassName('close')[0];
+
+button.onclick = function(){
+  loginBox.style.display = 'block';
+}
+
+span.onclick = function(){
+  loginBox.style.display = 'none';
+}
+
 
 // login function, directs the user to the right content.
-function loginChecker(form){
-  if(form.userid.value == '' || form.passwordid.value == ''){
+function loginCheck(form){
+  if(form.userId.value == '' || form.passwordId.value == ''){
     return alert('Sorry there are empty fields');
   }
   for(let i = 0; i < users.length; i++){
-    if(form.userid.value === users[i].username && form.passwordid.value === users[i].password){
-      if(form.userid.value === 'student'){
-        return window.location.replace('?');
-    } else if(form.userid.value === 'teacher'){
+    if(form.userId.value === users[i].username && form.passwordId.value === users[i].password){
+      if(form.userId.value === 'student'){
+        return window.location.replace('courses.html');
+    } else if(form.userId.value === 'teacher'){
         return window.location.replace('?');
       }
     }
