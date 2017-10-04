7703de43ab18433913b221fdb05dbcce9973588e
fadi <fadi.gourie@student.kyh.se>
2017-10-04 14:30:08 +0200

    Add JQuery script for shrinking and sticky navbar

--- js/script.js
+++ js/script.js
@@ -21,3 +21,26 @@ function loginChecker(form){
     }
   }  alert('Sorry invalid username or password');
 }
+
+(function($) {
+    "use strict";
+
+    var $navbar = $("#navbar"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > y_pos) {
+            $navbar.addClass("sticky").animate({
+                top: 0
+            });
+        } else if (scrollTop <= y_pos) {
+            $navbar.removeClass("sticky").clearQueue().animate({
+                top: "-48px"
+            }, 0);
+        }
+    });
+
+})(jQuery, undefined);
