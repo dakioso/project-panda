74c5989060cfa1d338a6025d227783328a2ef0cb
Mikael Gustafsson <mikael.gustafsson@student.kyh.se>
2017-10-23 11:47:41 +0200

    clean up code, add feature modal functions

--- js/script.js
+++ js/script.js
@@ -4,18 +4,26 @@ let users = [
   {username: 'teacher', password: 'password2'}
 ];
 
+// login-button function opens and close login-box
 let button = document.getElementById('loginbutton'),
     loginBox = document.getElementById('login-box'),
-    span = document.getElementsByClassName('close')[0];
+    span = document.getElementsByClassName('close')[0],
+    hiddenLogin = true;
 
 button.onclick = function(){
-  loginBox.style.display = 'block';
+  if(hiddenLogin){
+    loginBox.style.display = 'block';
+    hiddenLogin = false;
+  } else {
+      loginBox.style.display = 'none';
+      hiddenLogin = true;
+  }
 }
 
 span.onclick = function(){
   loginBox.style.display = 'none';
+  hiddenLogin = true;
 }
-
 
 // login function, directs the user to the right content.
 function loginCheck(form){
@@ -33,6 +41,25 @@ function loginCheck(form){
   }  alert('Sorry invalid username or password');
 }
 
+// open the feature-box
+let featureBox = document.getElementById('feature'),
+    btn = document.getElementById("compare"),
+    span1 = document.getElementsByClassName("close")[1];
+
+btn.onclick = function() {
+  featureBox.style.display = "block";
+}
+
+span1.onclick = function() {
+  featureBox.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == featureBox) {
+    featureBox.style.display = "none";
+  }
+}
+
 //navbar open and close.
 function togNav() {
   var nav = document.getElementById("nav");
@@ -43,25 +70,4 @@ function togNav() {
     nav.style.height = "100%";
    // nav.style.opacity = 1
  }
-}
-
-let featureBox = document.getElementById('feature'),
-    btn = document.getElementById("compare"),
-    span1 = document.getElementsByClassName("close")[1];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    featureBox.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
-    featureBox.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == featureBox) {
-        featureBox.style.display = "none";
-    }
 }
