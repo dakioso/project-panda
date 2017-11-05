18c77e35576b32d057d415349ee3ba611cd562d9
David Hansson <david.hansson@student.kyh.se>
2017-11-05 16:30:03 +0100

    Adding so teacher can add assigments through popup modalbox.

--- js/teacher.js
+++ js/teacher.js
@@ -317,3 +317,41 @@ function drawWeek(){
         green.restore();
 
 }
+
+
+// Get the modal
+var modal = document.getElementById('modalteacher');
+
+// Get the button that opens the modal
+var taskbutton = document.getElementById("opentask");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closetask")[0];
+
+
+
+// When the user clicks the button, open the modal
+taskbutton.onclick = function() {
+modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function teacherinput() {
+  	modal.style.display = "none";
+	   notif.innerText = "Tack för din åsikt.";
+    notif.style.color = '#3dd43d';
+    notif.style.borderColor ="#3dd43d";
+    setInterval(addHideMe, 5000);
+    dailyInvoke = true;
+}
