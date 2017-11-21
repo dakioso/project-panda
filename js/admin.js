d9fd0602a7e08bd23b4794979559cc63a0ff0444
dakioso <daniel.milojevic@student.kyh.se>
2017-11-21 10:16:20 +0100

    Add new js file. Notification functions when you create new user on admin site.

---
+++ js/admin.js
@@ -0,0 +1,29 @@
+var fnamntest = document.getElementById("fnamn");
+var enamn = document.getElementById("enamn");
+var datum = document.getElementById("datum");
+var klass = document.getElementById("klass");
+var anvnamn = document.getElementById("anvnamn");
+var lösen = document.getElementById("lösen");
+
+let notif = document.getElementById("notification");
+
+function reg() {
+  fnamntest.value = "";
+  enamn.value = "";
+  datum.value = "";
+  klass.value = "";
+  anvnamn.value = "";
+  lösen.value = "";
+  notif.innerText = "Ny användare skapad!";
+  notif.style.color = "#3dd43d";
+  notif.style.borderColor ="#3dd43d";
+  notif.classList.remove("hideMe");
+  setInterval(addHideMe, 1000);
+}
+
+
+
+function addHideMe() {
+  notif.classList.add("hideMe");
+
+}
