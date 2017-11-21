4064265f7f656f4df63377cd1ea7c0cb1bec7fca
tullinge1996 <31662409+tullinge1996@users.noreply.github.com>
2017-11-21 18:15:21 +0100

    added modulbox in teacher.html

--- js/teacher.js
+++ js/teacher.js
@@ -428,9 +428,43 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-
 function teacherinput() {
   	modal.style.display = "none";
+	   notif.innerText = "Uppgifterna är nu tillagda.";
+    notif.style.color = '#3dd43d';
+    notif.style.borderColor ="#3dd43d";
+    setInterval(addHideMe, 5000);
+    dailyInvoke = true;
+}
+// Get the modal2
+var modal2 = document.getElementById('modalteacher2');
+
+// Get the button that opens the modal
+var taskbutton2 = document.getElementById("opentask2");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("closetask2")[0];
+
+
+
+// When the user clicks the button, open the modal
+taskbutton2.onclick = function() {
+modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+function teacherinput2() {
+  	modal2.style.display = "none";
 	   notif.innerText = "Uppgifterna är nu tillagda.";
     notif.style.color = '#3dd43d';
     notif.style.borderColor ="#3dd43d";
