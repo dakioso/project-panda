09db7f051a55e184f324a8f12610a6f1e5a788a0
David Hansson <david.hansson@student.kyh.se>
2017-11-24 16:46:23 +0100

    cleaning up some code

--- js/student.js
+++ js/student.js
@@ -1,43 +1,83 @@
-// Get the modal
-var modal = document.getElementById('mymodal');
+/*------------------------------------------------------------------------
+                 Popup-box (Weekly Review) & (assignments) 
+------------------------------------------------------------------------*/
+// Popup-box Weekly Review
+var Reviewbox = document.getElementById('Reviewbox');
 
-// Get the button that opens the modal
+// IMG button to open  Popup-box Weekly Review
 var reviewbutton = document.getElementById("openreview");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closereview")[0];
+var closereviewbox = document.getElementsByClassName("closereview")[0];
 
+// When the user clicks on (x), close the Popup-box
+closereviewbox.onclick = function() {
+    Reviewbox.style.display = "none";
+}
 
-//attendance DOM
-let attendance = document.getElementById("attendanceCode");
-let attendanceReg = false;
-let notif = document.getElementById("notification");
-
-
-// When the user clicks the button, open the modal
+// When the user clicks the button, open the Reviewbox
 reviewbutton.onclick = function() {
-modal.style.display = "block";
+Reviewbox.style.display = "block";
 }
-
-// daily vote dom
-let happy = document.getElementById('happyF'),
-    okey = document.getElementById('neutralF'),
-    sad = document.getElementById('sadF'),
-    dailyInvoke = false;
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the Reviewbox, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == Reviewbox) {
+        Reviewbox.style.display = "none";
     }
 }
 
+// When the user send review, show input
+function reviewinput() {
+  	Reviewbox.style.display = "none";
+	notif.innerText = "Tack för din åsikt.";
+    notif.style.color = '#3dd43d';
+    notif.style.borderColor ="#3dd43d";
+    setInterval(addHideMe, 5000);
+    dailyInvoke = true;
+}
+/*------------------------------------------------------------------------*/
+
+//  Popup-box Weekly Review
+var Assigmentbox = document.getElementById('Assigmentbox');
+
+// navbar button to open Popup-box Assigment
+var openAssigment = document.getElementById("opentask");
+
+// Get the <span> element that closes the Assigmentbox
+var closeAssigment = document.getElementsByClassName("closetask")[0];
+
+
+// When the user clicks the button, open the Assigmentbox
+ openAssigment.onclick = function() {
+ Assigmentbox.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the Assigmentbox
+ closeAssigment.onclick = function() {
+ Assigmentbox.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the Assigmentbox, close it
+window.onclick = function(event) {
+    if (event.target == Assigmentbox) {
+        Assigmentbox.style.display = "none";
+    }
+}
+// Input when user send/add assigment,show input
+function teacherinput() {
+  	Assigmentbox.style.display = "none";
+	   notif.innerText = "Uppgifterna är nu tillagda.";
+    notif.style.color = '#3dd43d';
+    notif.style.borderColor ="#3dd43d";
+    setInterval(addHideMe, 5000);
+    dailyInvoke = true;
+}
+
+/*------------------------------------------------------------------------
+                Popup-box (Weekly Review) & (assignments)  END
+------------------------------------------------------------------------*/
+  
+ // Navbar 
   function togNav() {
   var nav = document.getElementById("nav");
   if (nav.style.height == '100%') {
@@ -46,44 +86,17 @@ window.onclick = function(event) {
     nav.style.height = "100%";
   }
 }
-// Get the modal
-var modal = document.getElementById('modalteacher');
 
-// Get the button that opens the modal
-var taskbutton = document.getElementById("opentask");
+//attendance DOM
+let attendance = document.getElementById("attendanceCode");
+let attendanceReg = false;
+let notif = document.getElementById("notification");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closetask")[0];
-
-
-
-// When the user clicks the button, open the modal
-taskbutton.onclick = function() {
-modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-function teacherinput() {
-  	modal.style.display = "none";
-	   notif.innerText = "Uppgifterna är nu tillagda.";
-    notif.style.color = '#3dd43d';
-    notif.style.borderColor ="#3dd43d";
-    setInterval(addHideMe, 5000);
-    dailyInvoke = true;
-}
-
-
+// daily vote dom
+let happy = document.getElementById('happyF'),
+    okey = document.getElementById('neutralF'),
+    sad = document.getElementById('sadF'),
+    dailyInvoke = false;
 
 // Checks the attandance code, if correct then register attendance
 function attendanceCheck() {
@@ -110,7 +123,6 @@ function attendanceCheck() {
     setInterval(addHideMe, 1000);
   }
 }
-
 
 // Print out the daily evaluation
 function printMess(face){
@@ -165,11 +177,4 @@ function addHideMe() {
 
 }
 
-function utvard() {
-  	modal.style.display = "none";
-	   notif.innerText = "Tack för din åsikt.";
-    notif.style.color = '#3dd43d';
-    notif.style.borderColor ="#3dd43d";
-    setInterval(addHideMe, 5000);
-    dailyInvoke = true;
-}
+
